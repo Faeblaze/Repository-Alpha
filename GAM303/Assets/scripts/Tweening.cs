3695fd020041c774efdabda5c7c0ecc91088b8c2
Faeblaze <33320058+Faeblaze@users.noreply.github.com>
2018-09-26 16:05:30 +1000

    Tweening
    learning week 1 rapid prototyping

---
+++ GAM303/Assets/scripts/Tweening.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Tweening : MonoBehaviour {
+
+
+
+    public GameObject cube;
+    public float tweenSpeed = 2f;
+	
+	
+	// Update is called once per frame
+	void Update ()
+
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            cube.transform.DOScale(4, tweenSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            cube.transform.DOMoveX(10, tweenSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            cube.transform.DOPunchPosition(Vector3.forward*10, tweenSpeed,1,1f);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Camera.main.DOShakePosition(tweenSpeed, 3, 5, 45);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            cube.GetComponent<Renderer>().material.DOColor(Color.blue, tweenSpeed);
+        }
+    }
+}
